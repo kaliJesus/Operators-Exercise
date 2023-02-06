1ca702cdf7fffd721b8c3486cf9697b78d704a99
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-02-06 11:16:56 -0500

    new committ

---
+++ OperatorExercise/exercisetwo.cs
@@ -0,0 +1,21 @@
+﻿namespace OperatorExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient}, and the remainder is {remainder}");
+            }
+        }
+            
+        
+        
+       
+    }
+}
