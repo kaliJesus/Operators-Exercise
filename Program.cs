51401541664250e4ed7686a7faa5ec6a6c3edb64
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-02-06 11:37:23 -0500

    neew

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercise22
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first radius: ");
+            var radius = double.Parse(Console.ReadLine());
+            double result = classone.AreaOfCircle(radius);
+            Console.WriteLine(result);
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
+        }
+
+
+
+
+    }
+}
+
